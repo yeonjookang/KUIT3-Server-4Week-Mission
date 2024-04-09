aeefc5682c42af6a201ce22be8ac274d5cb833ef
kang yeon joo <rkdduswndi@naver.com>
2024-04-09 16:17:44 +0900

    feat: ModelAndView 적용

--- src/main/java/core/mvc/view/JspView.java
+++ src/main/java/core/mvc/view/JspView.java
@@ -3,6 +3,8 @@ package core.mvc.view;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.Set;
 
 public class JspView implements View{
     private final String viewName;
@@ -13,11 +15,17 @@ public class JspView implements View{
     }
 
     @Override
-    public void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(Map<String,Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         if (viewName.startsWith(REDIRECT_PREFIX)) {
             response.sendRedirect(viewName.substring(REDIRECT_PREFIX.length()));
             return;
         }
+        Set<String> keys = model.keySet();
+
+        for(String key : keys){
+            request.setAttribute(key,model.get(key));
+        }
+
         RequestDispatcher rd = request.getRequestDispatcher(viewName);
         rd.forward(request, response);
     }
